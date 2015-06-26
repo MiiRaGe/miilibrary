7e390c2e8b41167dd2cf0e5becf799e043de1d56
Alexis Durand <alexis.durand@iwoca.co.uk>
2015-06-26 22:10:40 +0100

    added feedentries

---
+++ mii_rss/migrations/0002_feedentries.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mii_rss', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='FeedEntries',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('json_entries', models.TextField()),
+                ('date', models.DateTimeField(default=datetime.datetime(2015, 6, 26, 20, 42, 31, 349515, tzinfo=utc))),
+            ],
+            options={
+                'get_latest_by': 'date',
+            },
+        ),
+    ]
