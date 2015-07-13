2522f13528fc9fbb8cb661c97be0002a5c78e41f
Alexis Durand <alexis.durand@iwoca.co.uk>
2015-07-13 20:56:38 +0100

    removed linked to mongo

--- settings/test.py
+++ settings/test.py
@@ -6,5 +6,4 @@ NAS_ROOT = ''
 
 SOURCE_FOLDER = relative('tests/test_input/')
 DESTINATION_FOLDER = relative('tests/test_output/')
-MONGO_DB_NAME += '_test'
 DATABASES['default']['engine'] = 'django.db.backends.sqlite3'
