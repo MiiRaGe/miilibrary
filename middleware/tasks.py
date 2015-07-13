ae1ea560ab774692d9d96edd6f20d9756edf48c8
Alexis Durand <alexis.durand@iwoca.co.uk>
2015-07-13 23:50:57 +0100

    added task to backup db

---
+++ middleware/tasks.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+from celery import task
+from dbbackup.management.commands.dbbackup import Command
+
+@task(serializer='json')
+def db_backup():
+    Command().handle()
