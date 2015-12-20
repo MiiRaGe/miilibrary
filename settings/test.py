6c5caf9c06e86ee98b3b4c471b371f35c19ccfcf
Alexis Durand <alexis.durand@iwoca.co.uk>
2015-12-20 21:50:00 +0100

    removed local int est

--- settings/test.py
+++ settings/test.py
@@ -1,9 +1,3 @@
-from local import *
+import django
 
-REMOTE_FILE_OPERATION_ENABLED = False
-LOCAL_ROOT = ''
-NAS_ROOT = ''
-
-SOURCE_FOLDER = relative('tests/test_input/')
-DESTINATION_FOLDER = relative('tests/test_output/')
 DATABASES['default']['engine'] = 'django.db.backends.sqlite3'
