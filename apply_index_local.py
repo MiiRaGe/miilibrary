1f9a07b97c77131c49fe9f990377e52623a3c579
Alexis Durand <alexis.durand@iwoca.co.uk>
2016-01-01 13:29:50 +0000

    74% wohou...

--- apply_index_local.py
+++ apply_index_local.py
@@ -6,14 +6,21 @@ import settings
 
 from mii_common import tools
 
-json_file_path = os.path.join(settings.DESTINATION_FOLDER, settings.DUMP_INDEX_JSON_FILE_NAME)
-if __name__ == '__main__' and os.path.exists(json_file_path):
+
+def apply_index(path, json_file_name):
+    json_file_path = os.path.join(path, json_file_name)
+    if not os.path.exists(json_file_path):
+        return
     with open(json_file_path, 'rb') as inputfile:
         dict_index = json.load(inputfile)
 
-    index_path = os.path.join(settings.DESTINATION_FOLDER, 'Movies', 'Index')
+    index_path = os.path.join(path, 'Movies', 'Index')
     if os.path.exists(index_path):
         shutil.rmtree(index_path)
     current_path_root = tools.make_dir(index_path)
     tools.dict_apply(current_path_root, dict_index, symlink_method=os.symlink)
     os.remove(json_file_path)
+
+
+if __name__ == '__main__':
+    apply_index(settings.DESTINATION_FOLDER, settings.DUMP_INDEX_JSON_FILE_NAME)
---
+++ tests/test_apply_index.py
@@ -0,0 +1,38 @@
+import json
+import mock
+import os
+
+from django.conf import settings
+from django.test import override_settings
+
+from apply_index_local import apply_index
+from mii_common import tools
+from utils.base import TestMiilibrary
+
+
+@override_settings(DUMP_INDEX_JSON_FILE_NAME='lol.json')
+class TestApplyIndex(TestMiilibrary):
+    @mock.patch('apply_index_local.json.loads')
+    def test_return_immediately(self, loads):
+        apply_index(self.DESTINATION_FOLDER, settings.DUMP_INDEX_JSON_FILE_NAME)
+        assert not loads.called
+
+    @mock.patch('apply_index_local.tools.dict_apply')
+    def test_apply_index(self, dict_apply):
+        fake_index = {
+            'Actor': {
+                'Michael Douglas':
+                    [
+                        ('Thor.(2011).mkv', self.SOURCE_FOLDER + 'Thor.(2011).mkv'),
+                        ('Thor.(2011).720p.mkv', self.SOURCE_FOLDER + 'Thor.(2011).720p.mkv'),
+                    ]
+            }
+        }
+        index_path = os.path.join(self.DESTINATION_FOLDER, settings.DUMP_INDEX_JSON_FILE_NAME)
+        self.fs.CreateFile(index_path,
+                           contents=json.dumps(fake_index))
+        tools.make_dir(os.path.join(self.DESTINATION_FOLDER, 'Movie', 'Index'))
+        assert os.path.exists(index_path)
+        apply_index(self.DESTINATION_FOLDER, 'lol.json')
+        assert dict_apply.called
+        assert not os.path.exists(index_path)
