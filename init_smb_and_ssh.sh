e99722ddb77911e68756f8a75cf5f9756d51a9db
Alexis Durand <alexis.durand@iwoca.co.uk>
2016-03-13 12:24:53 +0000

    mounting fixed

--- init_smb_and_ssh.sh
+++ init_smb_and_ssh.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-if [ $SMB_LOGIN != "" && $SMB_PASSWORD != "" && $MOUNT_FOLDER != "" && $SMB_PATH != "" ];
+if [[ $SMB_LOGIN && $SMB_PASSWORD && $MOUNT_FOLDER && $SMB_PATH ]];
 then
  echo "Mounting device...";
  mount -t cifs $SMB_PATH $MOUNT_FOLDER -o user=$SMB_LOGIN,pass=$SMB_PASSWORD
+else
+ echo "SMB detail not found, not mounting network drive"
 fi;
 
 echo "Running ssh deamon";
