167a7674bf025439e78fc65361167253dc992e72
Alexis Durand <alexis.durand@iwoca.co.uk>
2016-03-13 11:39:49 +0000

    modified the init script name

---
+++ init_smb_and_ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $SMB_LOGIN != "" && $SMB_PASSWORD != "" && $MOUNT_FOLDER != "" && $SMB_PATH != "" ];
+then
+ echo "Mounting device...";
+ mount -t cifs $SMB_PATH $MOUNT_FOLDER -o user=$SMB_LOGIN,pass=$SMB_PASSWORD
+fi;
+
+/usr/sbin/sshd &
