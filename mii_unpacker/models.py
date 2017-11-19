938fb85551c2f396c932071099b97a9aa0f2bee4
Alexis Durand <alexis.durand@iwoca.co.uk>
2017-11-19 21:21:43 +0100

    added time to unpacked to track the latest

--- mii_unpacker/models.py
+++ mii_unpacker/models.py
@@ -1,7 +1,9 @@
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, DateTimeField
+from django.utils import timezone
 
 __author__ = 'MiiRaGe'
 
 
 class Unpacked(Model):
     filename = CharField(max_length=255, unique=True)
+    timestamp = DateTimeField(default=timezone.now)
