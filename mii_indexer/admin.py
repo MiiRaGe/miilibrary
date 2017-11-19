2ec30171939ddbb7cb562c18bcd905be585eb136
Alexis Durand <alexis.durand@iwoca.co.uk>
2017-11-19 22:10:44 +0100

    Added proper detail list

--- mii_indexer/admin.py
+++ mii_indexer/admin.py
@@ -1,8 +1,28 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib.admin import site
 from mii_indexer.models import MovieRelation, Person, MovieTagging, Tag
 
 __author__ = 'MiiRaGe'
 
+
+class MovieRelationAdmin(ModelAdmin):
+    model = MovieRelation
+    list_display = ('movie', 'person', 'type')
+
+
+class PersonAdmin(ModelAdmin):
+    model = Person
+    list_display = ('name',)
+
+
+class MovieTaggingAdmin(ModelAdmin):
+    model = MovieTagging
+    list_display = ('movie', 'tag')
+
+
+class TagAdmin(ModelAdmin):
+    model = Tag
+    list_display = ('name',)
 
 site.register(MovieRelation)
 site.register(Person)
--- mii_unpacker/admin.py
+++ mii_unpacker/admin.py
@@ -1,7 +1,13 @@
+from django.contrib.admin import ModelAdmin
 from django.contrib.admin import site
 from mii_unpacker.models import Unpacked
 
 __author__ = 'MiiRaGe'
 
 
-site.register(Unpacked)
+class UnpackedAdmin(ModelAdmin):
+    model = Unpacked
+    list_display = ('filename', 'timestamp')
+
+
+site.register(Unpacked, UnpackedAdmin)
