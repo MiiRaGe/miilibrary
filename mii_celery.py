db6fcd5bb630939ce323b4f524af7abf452bb9ee
Alexis <alexis.durand28@gmail.com>
2018-01-26 17:02:13 +0100

    TG-1 Updated stuf

--- mii_celery.py
+++ mii_celery.py
@@ -16,7 +16,7 @@ app = Celery('mii_celery')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(settings.INSTALLED_APPS)
 
-
+# Hope this works
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
