b244811a3072ed9c0c435577e97c06adc9c006f0
MiiRaGe <aptd@protonmail.com>
2023-02-07 09:55:37 +0100

    Update unittest2 to unittest

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,7 +2,7 @@ import mock
 import os
 
 from pyfakefs.fake_filesystem_unittest import TestCase as FakeFSTestCase
-from unittest2 import TestCase
+from unittest import TestCase
 
 from mii_common.tools import make_dir, delete_dir, listdir_abs, dict_apply
 
