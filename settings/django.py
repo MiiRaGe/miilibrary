37a5ca9439fd0c639965e6f6cae1df598caed5a8
MiiRaGe <aptd@protonmail.com>
2023-02-07 14:33:26 +0100

    Add request context processor.

--- settings/django.py
+++ settings/django.py
@@ -121,6 +121,7 @@ TEMPLATES = [
         ],
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
